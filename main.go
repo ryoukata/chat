7b82ba3bbcdbba8c71b95c0c353151fd8aa09ce4
ryoukata <ryoya3091@gmail.com>
2021-02-27 16:13:03 +0900

    fix(#1): use template for http file.

--- main.go
+++ main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>Chat</title>
-				</head>
-				<body>
-					Let's Chat!
-				</body>
-			</html>
-		`))
+// templ return one template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServerHttp execute HTTP Request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	// root
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// start Web Server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
